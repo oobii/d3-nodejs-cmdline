f59d2dbd03b8fc1e6abd6c91da1632d573eeb3b2
martynov <vvayfarer@yandex.com>
2017-10-06 20:29:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# d3-nodejs-cmdline
---
+++ app.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+var d3 = require("d3");
+
+//D3's data loading functionality is based on XMLHttpRequest
+// we use file system functionality to load the data
+fs.readFile("/Users/martynov/WebstormProjects/d3-test1/cities.csv", "utf8", function(error, data) {
+	  data = d3.tsvParse(data);
+	    console.log(JSON.stringify(data));
+
+// converting string values to numbers by adding '+'	    
+data.forEach(function(d) {
+	    d.population = +d.population;
+	        d["land area"] = +d["land area"];
+		  });
+  console.log(data);
+});
